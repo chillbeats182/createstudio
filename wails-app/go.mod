03df095683aca920fdae387ba1ecc64a9515fa40
Z User <z@container>
2026-07-15 05:25:26 +0000

    fix: remove unused context import + update Wails to v2.13.0
    - Remove unused 'context' import in main.go (caused compile error)
    - Update go.mod Wails version from 2.9.1 to 2.13.0 to match CLI

--- wails-app/go.mod
+++ wails-app/go.mod
@@ -2,4 +2,4 @@ module oreateai-studio
 
 go 1.22
 
-require github.com/wailsapp/wails/v2 v2.9.1
+require github.com/wailsapp/wails/v2 v2.13.0
--- wails-app/main.go
+++ wails-app/main.go
@@ -1,38 +1,37 @@
 package main
 
 import (
-	"context"
-	"embed"
-	"log"
+        "embed"
+        "log"
 
-	"github.com/wailsapp/wails/v2"
-	"github.com/wailsapp/wails/v2/pkg/options"
-	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+        "github.com/wailsapp/wails/v2"
+        "github.com/wailsapp/wails/v2/pkg/options"
+        "github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
 
 //go:embed all:frontend
 var assets embed.FS
 
 func main() {
-	app := NewApp()
+        app := NewApp()
 
-	err := wails.Run(&options.App{
-		Title:     "OreateAI Studio",
-		Width:     1280,
-		Height:    820,
-		MinWidth:  960,
-		MinHeight: 640,
-		AssetServer: &assetserver.Options{
-			Assets: assets,
-		},
-		BackgroundColour: &options.RGBA{R: 255, G: 255, B: 255, A: 255},
-		OnStartup:        app.startup,
-		Bind: []interface{}{
-			app,
-		},
-	})
+        err := wails.Run(&options.App{
+                Title:     "OreateAI Studio",
+                Width:     1280,
+                Height:    820,
+                MinWidth:  960,
+                MinHeight: 640,
+                AssetServer: &assetserver.Options{
+                        Assets: assets,
+                },
+                BackgroundColour: &options.RGBA{R: 255, G: 255, B: 255, A: 255},
+                OnStartup:        app.startup,
+                Bind: []interface{}{
+                        app,
+                },
+        })
 
-	if err != nil {
-		log.Fatalf("Failed to start application: %v", err)
-	}
+        if err != nil {
+                log.Fatalf("Failed to start application: %v", err)
+        }
 }
